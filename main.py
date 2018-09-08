1de13bdc03d5619ff50a1651e25b65b0557e30ad
xhn16729 <xiong_haonan@163.com>
2018-09-08 15:03:30 +0800

    finished

--- main.py
+++ main.py
@@ -9,20 +9,22 @@ from telebot import types
 
 token = open("config", "r").read()
 group_id = open("group_id", "r").read().split()
-group_mates = open("group_mates", "r").read().split()
 
-
-
-'''bot = telebot.Telebot("token")'''
+bot = telebot.Telebot("token")
 
 @bot.message_handler(commands=['goodnight'])
 def goodnight(message):
-	if message.chat.id in group_id:
-	txt = "晚安"
-	for i in range(0, len(group_mates)):
-		txt = txt + "@" + group_mates[i] + " "
-	bot.reply_to(message, txt)
-
+	print(message.chat.id)
+	#print(message)
+	if str(message.chat.id) in group_id:
+		if message.reply_to_message == None:
+			txt = str(message.from_user.first_name)
+			txt = txt + "向大家道晚安~"
+			bot.send_message(message.chat.id, txt)
+		else:
+			txt = str(message.from_user.first_name)
+			txt = txt + "向" + str(message.reply_to_message.from_user.first_name) + "道晚安~"
+			bot.send_message(message.chat.id, txt)
 
 
 bot.polling()
