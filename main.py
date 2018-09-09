8d27a5ba18f0d65b769e3838fe6a26a5b8a526f3
xhn16729 <xiong_haonan@163.com>
2018-09-09 15:42:40 +0800

    auto delete command msg

--- main.py
+++ main.py
@@ -3,7 +3,10 @@
 
 import os
 import sys
+sys.path.append("/usr/local/python3.6/lib/python3.6/site-packages")
+sys.path.append("/usr/local/lib/python3.5/dist-packages")
 import telebot
+import time
 from telebot import types
 
 
@@ -12,19 +15,32 @@ group_id = open("group_id", "r").read().split()
 
 bot = telebot.TeleBot(token)
 
-@bot.message_handler(commands=['goodnight'])
-def goodnight(message):
+@bot.message_handler(commands=['greeting'])
+def greeting(message):
 	print(message.chat.id)
-	#print(message)
 	if str(message.chat.id) in group_id:
+		c_time = time.time() + 28800
+		c_time = int(c_time % 86400 // 3600)
+		if 0<=c_time<4:
+			txt = "晚安"
+		elif 4<=c_time<10:
+			txt = "早上好"
+		elif 10<=c_time<15:
+			txt = "中午好"
+		elif 15<=c_time<18:
+			txt = "下午好"
+		elif 18<=c_time<21:
+			txt = "晚上好"
+		elif 21<=c_time<24:
+			txt = "晚安"
+		send_name = str(message.from_user.first_name)
 		if message.reply_to_message == None:
-			txt = str(message.from_user.first_name)
-			txt = txt + "向大家道晚安~"
-			bot.send_message(message.chat.id, txt)
+			txt = send_name + "向大家道" + txt + "～"
 		else:
-			txt = str(message.from_user.first_name)
-			txt = txt + "向" + str(message.reply_to_message.from_user.first_name) + "道晚安~"
-			bot.send_message(message.chat.id, txt)
+			reply_name = str(message.reply_to_message.from_user.first_name)
+			txt = send_name + "向" + reply_name + "道" + txt + "～"
+		bot.delete_message(message.chat.id, message.message_id)
+		bot.send_message(message.chat.id, txt)
 
 
 bot.polling()
