3a1dd5359af11c3daeae97e0e6f4be4d5df11278
xhn16729 <xiong_haonan@163.com>
2019-01-05 18:52:58 +0800

    add nickname feature

--- main.py
+++ main.py
@@ -9,14 +9,18 @@ import telebot
 import time
 from telebot import types
 
-
 bot = telebot.TeleBot("REPLACE YOUR TOKEN HERE")
+
+custom_userid = [400521524, 407635222, 223347749, 638996316, 299143063, 459094099]
+custom_nickname = ['小熊', 'Lore酱', '47', '47', '荔枝', 'Yunhao']
 
 try:
 	@bot.message_handler(commands=['greeting'])
 	def greeting(message):
+
 		c_time = time.time() + 28800
 		c_time = int(c_time % 86400 // 3600)
+
 		if 0<=c_time<5:
 			txt = "晚安"
 		elif 5<=c_time<11:
@@ -29,17 +33,29 @@ try:
 			txt = "晚上好"
 		elif 22<=c_time<24:
 			txt = "晚安"
-		send_name = str(message.from_user.first_name)
-		if message.reply_to_message == None:
-			txt = send_name + " 向 大家 道" + txt + "～"
+
+		if message.from_user.id in custom_userid:
+			send_name = custom_nickname[custom_userid.index(message.from_user.id)]
 		else:
-			reply_name = str(message.reply_to_message.from_user.first_name)
+			send_name = str(message.from_user.first_name)
+
+		if message.reply_to_message == None:
+				txt = send_name + " 向 大家 道" + txt + "～"
+		else:
+			if message.reply_to_message.from_user.id in custom_userid:
+				reply_name = custom_nickname[custom_userid.index(message.reply_to_message.from_user.id)]
+			else:
+				reply_name = str(message.reply_to_message.from_user.first_name)
+
 			if message.reply_to_message.from_user.username == None:
 				txt = send_name + " 向 " + reply_name + " 道 " + txt + "～"
+			elif message.from_user.id == message.reply_to_message.from_user.id:
+				txt = "我 给 我 自 己 打 招 呼" 
+			elif message.reply_to_message.from_user.username == "goodnight_prpr_bot":
+				txt = "不需要 给窝 道 打招呼 啦～" 
 			else:
 				txt = send_name + " 向 " + reply_name + " 道 " + txt + "～ @" + message.reply_to_message.from_user.username
-			if message.reply_to_message.from_user.username == "goodnight_prpr_bot":
-				txt = "不需要 给窝 道 打招呼 啦～" 
+
 		bot.send_message(message.chat.id, txt)
 		bot.delete_message(message.chat.id, message.message_id)
 
