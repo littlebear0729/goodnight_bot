688d05d208dcfa1f53d8d7a4b2717bd39af6c59b
xhn16729 <xiong_haonan@163.com>
2019-01-05 18:57:57 +0800

    add comment

--- main.py
+++ main.py
@@ -9,18 +9,21 @@ import telebot
 import time
 from telebot import types
 
+# bot token
 bot = telebot.TeleBot("REPLACE YOUR TOKEN HERE")
 
+# custom nickname by detect userID
 custom_userid = [400521524, 407635222, 223347749, 638996316, 299143063, 459094099]
 custom_nickname = ['小熊', 'Lore酱', '47', '47', '荔枝', 'Yunhao']
 
 try:
 	@bot.message_handler(commands=['greeting'])
 	def greeting(message):
-
+		# get local time
 		c_time = time.time() + 28800
 		c_time = int(c_time % 86400 // 3600)
 
+		# change greeting text depend on time
 		if 0<=c_time<5:
 			txt = "晚安"
 		elif 5<=c_time<11:
@@ -34,32 +37,40 @@ try:
 		elif 22<=c_time<24:
 			txt = "晚安"
 
+		# if nickname recorded
 		if message.from_user.id in custom_userid:
 			send_name = custom_nickname[custom_userid.index(message.from_user.id)]
 		else:
 			send_name = str(message.from_user.first_name)
 
+		# if not reply to anyone
 		if message.reply_to_message == None:
 				txt = send_name + " 向 大家 道" + txt + "～"
 		else:
+			# if it is a reply message
+			# if nickname recorded
 			if message.reply_to_message.from_user.id in custom_userid:
 				reply_name = custom_nickname[custom_userid.index(message.reply_to_message.from_user.id)]
 			else:
 				reply_name = str(message.reply_to_message.from_user.first_name)
-
+			# if reply_to user doesn't have username
 			if message.reply_to_message.from_user.username == None:
 				txt = send_name + " 向 " + reply_name + " 道 " + txt + "～"
+			# if you reply to yourself
 			elif message.from_user.id == message.reply_to_message.from_user.id:
 				txt = "我 给 我 自 己 打 招 呼" 
+			# if you reply to the bot
 			elif message.reply_to_message.from_user.username == "goodnight_prpr_bot":
 				txt = "不需要 给窝 道 打招呼 啦～" 
+			# common reply
 			else:
 				txt = send_name + " 向 " + reply_name + " 道 " + txt + "～ @" + message.reply_to_message.from_user.username
-
+		# send reply and delete command message
 		bot.send_message(message.chat.id, txt)
 		bot.delete_message(message.chat.id, message.message_id)
 
 	bot.polling(none_stop=True)
+# catch exception
 except KeyboardInterrupt:
     quit()
 except Exception as e:
