5d743ab3f56e0c7831ac20f3c40e4a0c6a259ade
xhn16729 <xiong_haonan@163.com>
2019-06-13 20:25:46 +0800

    Add midnight easter egg

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ bot = telebot.TeleBot(bot_token)
 custom_userid = [400521524, 407635222, 223347749, 638996316, 299143063, 459094099, 254030480, 73322551]
 custom_nickname = ['小熊', 'Lore酱', '47', '47', '荔枝', 'mashiro', 'KingCapri', '柯柯']
 random_stickers = ['CAADBQADvgYAAvjGxQoD_y6N-wJ3BwI', 'CAADBQADOAYAAvjGxQrGnfgTD5nfwQI']
+sleep_reminder = ['您还没有睡觉吗？', '快去睡觉了啦！', '她已经睡着了喔…', '对方已经是守夜冠军了！', '你这么寂寞的嘛…', '[Google搜索：失眠怎么办](https://www.google.com/search?q=失眠怎么办)']
 
 try:
 	@bot.message_handler(commands=['greeting'])
@@ -33,9 +34,9 @@ try:
 		c_time = int(c_time % 86400 // 3600)
 
 		# change greeting text depend on time
-		if 0<=c_time<5:
-			txt = "晚安"
-		elif 5<=c_time<11:
+		if 0<=c_time<4:
+			txt = "睡觉"
+		elif 4<=c_time<11:
 			txt = "早安"
 		elif 11<=c_time<14:
 			txt = "中午好"
@@ -73,11 +74,14 @@ try:
 					txt = "[" + reply_name + "](tg://user?id=" + reply_id + ")～ [" + send_name + "](tg://user?id=" + from_id + ") 爱你哦～"
 				else:
 					txt = "[" + send_name + "](tg://user?id=" + from_id + ") 向 [" + reply_name + "](tg://user?id=" + reply_id + ") 道 " + txt + "～"
+			if txt == "睡觉":
+				randReminder = random.randint(0, len(sleep_reminder) - 1)
+				txt = sleep_reminder[randReminder]
 			else:
 				randNum = random.randint(0, 100)
 				if randNum % 6 == 0:
 					randStick = random.randint(0, len(random_stickers) - 1)
-					bot.send_sticker(message.chat_id, random_stickers[randStick])
+					bot.send_sticker(message.chat.id, random_stickers[randStick])
 				txt = "[" + send_name + "](tg://user?id=" + from_id + ") 向 [" + reply_name + "](tg://user?id=" + reply_id + ") 道 " + txt + "～"
 		# send reply and delete command message
 			bot.reply_to(message.reply_to_message, txt, parse_mode="Markdown")
