b422e477dd978a2542cbc0382e689f54b8091477
xhn16729 <xiong_haonan@163.com>
2019-07-21 20:46:34 +0800

    Add nickname for 菡
    mashiro 囍 菡

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ with open('./config.json', 'r+') as config_file:
 bot = telebot.TeleBot(bot_token)
 
 # custom nickname by detect userID
-custom_userid = [400521524, 407635222, 223347749, 638996316, 602231778, 459094099, 254030480, 73322551, 137105537, 557153656, 239887702]
-custom_nickname = ['小熊', 'Lore酱 | ☆可爱 verified by LittleBear', '47', '47', '荔枝', 'mashiro', 'KingCapri', '柯柯', '成本', '妹抖喵喵喵喵', 'Yooooooru']
+custom_userid = [400521524, 407635222, 223347749, 638996316, 602231778, 459094099, 254030480, 73322551, 137105537, 557153656, 239887702, 608795358]
+custom_nickname = ['小熊', 'Lore酱 | ☆可爱 verified by LittleBear', '47', '47', '荔枝', 'mashiro | 永远喜欢 菡', 'KingCapri', '柯柯', '成本', '妹抖喵喵喵喵', 'Yooooooru', '菡 | 永远喜欢 mashiro']
 random_stickers = ['CAADBQADvgYAAvjGxQoD_y6N-wJ3BwI', 'CAADBQADOAYAAvjGxQrGnfgTD5nfwQI']
 sleep_reminder = ['小可爱还没有睡觉吗？', '快去睡觉了啦！', '她已经睡着了喔…', '你的小可爱已经是守夜冠军了！']
 
